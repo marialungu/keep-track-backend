5419149750ef22fc989e15cd8f03876e4dc6b6d9
Maria Lungu <anauna.lungu@gmail.com>
2018-05-13 13:39:54 +0300

    deployment setup

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 --SELECT 1 FROM DUAL;
-INSERT INTO USERS (u_id, u_name, u_email, u_key, u_created_at) VALUES ('2', 'Nymeria Ghost', 'nymghost@gmail.com', 'asdfg12345', '2017-05-12');
+--INSERT INTO USERS (u_id, u_name, u_email, u_key, u_created_at) VALUES ('2', 'Nymeria Ghost', 'nymghost@gmail.com', 'asdfg12345', '2017-05-12');
 INSERT INTO BOARDS (b_id, b_name, b_color, b_created_at, b_u_id) VALUES ('1', 'Work', 'NULL', '2016-05-18', '2');
 INSERT INTO BOARDS (b_id, b_name, b_color, b_created_at, b_u_id) VALUES ('2', 'Default', 'NULL', '2016-05-02', '2');
 INSERT INTO BOARDS (b_id, b_name, b_color, b_created_at, b_u_id) VALUES ('3','Food', 'aqua', '2017-02-23', '2');
